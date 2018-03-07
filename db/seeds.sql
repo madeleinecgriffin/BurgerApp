ca79a751ee2c11c6fb8c71195376e3962e1d0a77
Madeleine Griffin <madeleinecgriffin@gmail.com>
2018-03-06 20:21:55 -0600

    working - just need to update css

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,5 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Blue Cheese Burger', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('California Turkey Burger', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Farm Fried Egg Burger', false);
